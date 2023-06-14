9747050cb366ce69c47f5073c35c85c5e7676f35
sunhitse <sunilakuthota@gmail.com>
2023-06-14 13:05:28 +0530

    Even

---
+++ src/Even/Even.java
@@ -0,0 +1,14 @@
+package Even;
+
+class Even {
+    public static void main(String[] args)
+    {
+        int num = 9;
+        if (num % 2 == 0) {
+            System.out.println("Entered Number is Even");
+        }
+        else {
+            System.out.println("Entered Number is Odd");
+        }
+    }
+}
